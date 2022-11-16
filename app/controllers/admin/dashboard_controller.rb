b6aff23068fc484e6561d7632f7a448abbdcecc1
nicohsfu <marcnicohernandez@gmail.com>
2022-11-16 15:56:26 -0800

    show counts on the dashboard page - num of items, products, categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @num_products = Product.count
+    @num_product_items = Product.sum('quantity')
+    @num_categories = Category.count
   end
 end
