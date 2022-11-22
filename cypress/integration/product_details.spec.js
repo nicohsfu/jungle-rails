665885aec3d04a49a24984f13809e9e8692fce98
nicohsfu <marcnicohernandez@gmail.com>
2022-11-22 13:06:04 -0800

    Feature Specs: Product Details and Add to Cart

--- cypress/integration/product_details.spec.js
+++ cypress/integration/product_details.spec.js
@@ -2,14 +2,9 @@ describe('product_details.spec.js', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
   it("should bring you to the product details page", () => {
     cy.get("article").first().click()
-  });
-  
-  it("should be on the product details page now", () => {
-    cy.visit('/products/2')
-    cy.get(".page-header").should("contain", "Evergreens » Scented Blade")
+    cy.url().should("include", "/products/2")
   });
   
 })
